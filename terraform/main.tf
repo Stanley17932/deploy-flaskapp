efd46d9e34ab6592f04ea9e8057123c1bf9a8fa1
Stanley17932 <stanlieds@gmail.com>
2025-08-29 09:59:25 +0300

    fix: handle existing infrastructure resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,94 +14,34 @@ provider "google" {
   region  = var.region
 }
 
-# Enable required APIs
-resource "google_project_service" "cloudrun_api" {
-  project = var.project_id
-  service = "run.googleapis.com"
-
-  disable_dependent_services = false
-  disable_on_destroy         = false
-}
-
-resource "google_project_service" "artifact_registry_api" {
-  project = var.project_id
-  service = "artifactregistry.googleapis.com"
-
-  disable_dependent_services = false
-  disable_on_destroy         = false
-}
-
-resource "google_project_service" "cloudbuild_api" {
-  project = var.project_id
-  service = "cloudbuild.googleapis.com"
-
-  disable_dependent_services = false
-  disable_on_destroy         = false
-}
-
-resource "google_project_service" "iam_api" {
-  project = var.project_id
-  service = "iam.googleapis.com"
-
-  disable_dependent_services = false
-  disable_on_destroy         = false
-}
-
-resource "google_project_service" "vpcaccess_api" {
-  project = var.project_id
-  service = "vpcaccess.googleapis.com"
-
-  disable_dependent_services = false
-  disable_on_destroy         = false
-}
-
 # Use existing Artifact Registry repository (deploy-flaskapp)
 data "google_artifact_registry_repository" "app_repo" {
   location      = var.region
   repository_id = var.artifact_registry_repo
-
-  depends_on = [google_project_service.artifact_registry_api]
 }
 
-# Create service account for Cloud Run
-resource "google_service_account" "cloudrun_sa" {
-  account_id   = "${var.app_name}-cloudrun-sa"
-  display_name = "Cloud Run Service Account for ${var.app_name}"
-  description  = "Service account for running the text analyzer Cloud Run service"
+# Use existing service accounts instead of creating new ones
+data "google_service_account" "cloudrun_sa" {
+  account_id = "${var.app_name}-cloudrun-sa"
 }
 
-# Grant minimal permissions to service account
-resource "google_project_iam_member" "cloudrun_sa_logging" {
-  project = var.project_id
-  role    = "roles/logging.logWriter"
-  member  = "serviceAccount:${google_service_account.cloudrun_sa.email}"
+data "google_service_account" "invoker_sa" {
+  account_id = "${var.app_name}-invoker-sa"
 }
 
-resource "google_project_iam_member" "cloudrun_sa_monitoring" {
-  project = var.project_id
-  role    = "roles/monitoring.metricWriter"
-  member  = "serviceAccount:${google_service_account.cloudrun_sa.email}"
+# Use existing VPC connector
+data "google_vpc_access_connector" "connector" {
+  name   = "${var.app_name}-connector"
+  region = var.region
 }
 
-# Create invoker service account for accessing the service
-resource "google_service_account" "invoker_sa" {
-  account_id   = "${var.app_name}-invoker-sa"
-  display_name = "Service Account for invoking ${var.app_name}"
-  description  = "Service account with permission to invoke the text analyzer service"
-}
-
-# Cloud Run service
+# Cloud Run service (this might need to be created or updated)
 resource "google_cloud_run_v2_service" "app_service" {
   name     = var.app_name
   location = var.region
 
-  depends_on = [
-    google_project_service.cloudrun_api,
-    google_service_account.cloudrun_sa
-  ]
-
   template {
-    service_account = google_service_account.cloudrun_sa.email
+    service_account = data.google_service_account.cloudrun_sa.email
 
     scaling {
       min_instance_count = 0
@@ -110,7 +50,7 @@ resource "google_cloud_run_v2_service" "app_service" {
 
     # VPC Access for internal networking
     vpc_access {
-      connector = google_vpc_access_connector.connector.id
+      connector = data.google_vpc_access_connector.connector.id
       egress    = "PRIVATE_RANGES_ONLY"
     }
 
@@ -173,18 +113,5 @@ resource "google_cloud_run_v2_service_iam_member" "invoker_access" {
   location = google_cloud_run_v2_service.app_service.location
   name     = google_cloud_run_v2_service.app_service.name
   role     = "roles/run.invoker"
-  member   = "serviceAccount:${google_service_account.invoker_sa.email}"
-}
-
-# VPC connector for secure networking
-resource "google_vpc_access_connector" "connector" {
-  name          = "${var.app_name}-connector"
-  region        = var.region
-  ip_cidr_range = "10.8.0.0/28"
-  network       = "default"
-
-  depends_on = [
-    google_project_service.cloudrun_api,
-    google_project_service.vpcaccess_api
-  ]
+  member   = "serviceAccount:${data.google_service_account.invoker_sa.email}"
 }
